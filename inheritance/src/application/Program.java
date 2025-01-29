6b6ccd883e721a8ee15317056a9bc9111f87304c
Gustavo Maia <ms.gustavo@outlook.com>
2025-01-29 19:43:10 -0300

    upcasting and downcasting

--- inheritance/src/application/Program.java
+++ inheritance/src/application/Program.java
@@ -1,10 +1,40 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Account acc = new Account(1001, "Gustavo Maia", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Beatriz Mota", 0.0, 500.0);
+		
+		// UPCASTING
+		// Subclasse atribuido para variavel da Superclasse
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Roque Carvalho", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Felipe Maia", 0.0, 0.01);
 
+		// DOWNCASTING
+		//Superclasse atribuido para variavel da Subclasse
+		BusinessAccount acc4 = (BusinessAccount) acc2;
+		acc4.loan(100);
+		
+//		BusinessAccount acc5 = (BusinessAccount) acc3;
+
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount) acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+		
 	}
 
 }
--- inheritance/src/entities/BusinessAccount.java
+++ inheritance/src/entities/BusinessAccount.java
@@ -11,6 +11,20 @@ public class BusinessAccount extends Account {
 	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
 		super(number, holder, balance);
 		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
 	};
+
+	public void loan(double amount) {
+		if (amount <= loanLimit) {
+			deposit(amount);
+		}
+	}
 
 }
